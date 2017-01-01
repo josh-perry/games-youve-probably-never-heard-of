fbead45380b72eabd61ff6adf95977983cc2ace8
Josh Perry <joshperry@outlook.com>
2017-01-01 14:14:28 +0000

    :sparkles: Downloads images and posts them in the tweet

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 import random
 import datetime as dt
+import urllib
+import urlparse
 
-from secrets import gb_api_key
+from secrets import *
 
 from giantbomb import giantbomb
+import tweepy
 
 useragent = "Test"
+
+# Maximum number of images to download
+max_images = 4
 
 
 def format_release_date(game):
@@ -38,7 +45,39 @@ def get_credits(game):
     return credit
 
 
-def main():
+def save_images(game):
+    base_path = "./cache/{}/".format(game.id)
+    image_paths = []
+
+    download_count = 0
+
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+    for i in xrange(len(game.images)):
+        if download_count >= max_images:
+            break
+
+        try:
+            image = game.images[i]
+
+            url = image["super_url"]
+
+            url_path = urlparse.urlparse(url).path
+            ext = os.path.splitext(url_path)[1]
+
+            path = base_path + str(i) + ext
+            urllib.urlretrieve(url, path)
+
+            image_paths.append(path)
+            download_count += 1
+        except:
+            continue
+
+    return image_paths
+
+
+def get_game():
     # Init GiantBomb API with our key/useragent string
     gb = giantbomb.Api(gb_api_key, useragent)
 
@@ -46,6 +85,10 @@ def main():
     # Get a random game
     game = gb.getGame(random.randint(1, 57500))
 
+    return game
+
+
+def create_tweet(game):
     # Get string for all platforms the game was released on
     platforms = get_platforms(game)
 
@@ -60,7 +103,20 @@ def main():
     # Get credits string for showing developer/publisher
     tweet += get_credits(game)
 
-    print(tweet)
+    return tweet
+
+
+def post_tweet(tweet, images):
+    # Set up Twitter stuff
+    twitter_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    twitter_auth.set_access_token(access_key, access_secret)
+    twitter_api = tweepy.API(twitter_auth)
+
+    # Upload media + get IDs for the status
+    media_ids = [twitter_api.media_upload(i).media_id_string for i in images]
+
+    # Post tweet!
+    twitter_api.update_status(status=tweet, media_ids=media_ids)
 
 
 if __name__ == '__main__':
@@ -68,7 +124,9 @@ if __name__ == '__main__':
 
     for i in range(retries):
         try:
-            main()
+            game = get_game()
+            tweet = create_tweet(game)
+            post_tweet(tweet, save_images(game))
             break
-        except Exception, x:
+        except x:
             print("Error: " + x.message + ", retrying")
